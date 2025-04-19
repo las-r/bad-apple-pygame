5f3fcf3021708f41640a3167646f784ad38f0754
las_r <128936184+las-r@users.noreply.github.com>
2025-04-18 21:55:41 -0400

    Add actual functionality

--- bad-apple.py
+++ bad-apple.py
@@ -3,10 +3,33 @@ import pygame
 
 # init
 pygame.init()
+clock = pygame.time.Clock()
 
 # settings
 WIDTH, HEIGHT = 1920, 1080
 
-# frames list
+# load song
+song = pygame.mixer.Sound("song.wav")
+
+# load frames
+print("loading frames...")
 frames = []
-for f in 
+for name in sorted(os.listdir("frames"), key=lambda x: int(os.path.splitext(x)[0])):
+    frames.append(pygame.transform.scale(pygame.image.load(os.path.join("frames", name)), (WIDTH, HEIGHT)))
+print("frames loaded")
+
+# setup display
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("bad apple")
+
+# play music
+song.play()
+
+# main loop
+for f in frames:
+    # show image on screen
+    screen.blit(f, (0, 0))
+    pygame.display.flip()
+    
+    # fps
+    clock.tick(29.97)
